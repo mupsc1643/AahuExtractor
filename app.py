7eef7ceb419e17ef4f256a95bba90018049a7fb0
TGWOLVES <subhashgodara237057@gmail.com>
2025-06-17 22:51:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-import os
 from flask import Flask
-
 app = Flask(__name__)
-PORT = int(os.environ.get("PORT", 5000))
 
-@app.route("/")
-def home():
-    return "Hello, Render!"
+@app.route('/')
+def hello_world():
+    return 'Hello from Tech VJ'
+
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=PORT)
+    app.run()
