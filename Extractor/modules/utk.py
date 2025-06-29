e456050e4f573eecff7b0380ad7b9e0bbf14bd34
saini19028 <sainisahab190202@gmail.com>
2025-06-29 09:38:12 +0530

    Update utk.py

--- Extractor/modules/utk.py
+++ Extractor/modules/utk.py
@@ -636,7 +636,7 @@ async def login(app, user_id, m, all_urls, start_time, bname, batch_id, progress
             f"├─ 📦 Others: {other_count}\n"
             f"└─ 🔐 Protected: {drm_count}\n\n"
             f"🚀 <b>Extracted by</b>: @{(await app.get_me()).username}\n\n"
-            f"<code>╾───• @PRO_TXT_EXTRATOR_BOT •───╼</code>"
+            f"╾───• @nothing_saini •───╼"
         )
         
         # Send file with thumbnail
