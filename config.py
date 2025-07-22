ab78528a66e88aca6438d6ba4bdd58240d32e31c
ifsabhi64 <ifsabhi64@gmail.com>
2025-07-23 00:25:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,34 +3,34 @@ from os import getenv
 
 
 # ------------------------------------------------
-API_ID = int(os.environ.get("API_ID", "22873037"))
+API_ID = int(os.environ.get("API_ID", "20567114"))
 # ------------------------------------------------
-API_HASH = os.environ.get("API_HASH","dfb366687851460a58461c5a50f5b")
+API_HASH = os.environ.get("API_HASH","8a5b92106e45fc6637a65a67df060a65")
 # ------------------------------------------------
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7835844710:AAE66mbYEhUU6nQ_eVd9LkKeJpblqE")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8090907579:AAE5bN3xZmCbuuj9-NH_IEbWjVpq6awfFrk")
 # ------------------------------------------------
-BOT_USERNAME = os.environ.get("BOT_USERNAME", "@PRO_TXT_EXTRATOR_BOT")
-BOT_TEXT = "@Saini_extractor_bot"
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "@AbhiTXT1bot")
+BOT_TEXT = "@AbhiTXT1bot"
 # ------------------------------------------------
-OWNER_ID = int(os.environ.get("OWNER_ID", "7326627068"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "8036182138"))
 # ------------------------------------------------
 # //LOG CHANNEL ID 
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002606919098"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002441127578"))
 
 # //FORCE_CHANNEL_ID
-CHANNEL_ID2 = int(os.environ.get("CHANNEL_ID2", "-1002684674627")) 
+CHANNEL_ID2 = int(os.environ.get("CHANNEL_ID2", "-1002441127578")) 
 # ------------------------------------------------
 MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://vikassonawale0:JWyQFas7vlG1bkaL@cluster0.beermge.mongodb.net/?retryWrites=true&w=majority")
 # -----------------------------------------------
-PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1002694695011"))
+PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1002441127578"))
 # -----------------------------------------------
-join = '<a href="https://t.me/nothing_saini">✳️ JOIN BACKUP</a>'
+join = '<a href="https://t.me/ifsashu1">✳️ JOIN BACKUP</a>'
 # -----------------------------------------------
 UNSPLASH_ACCESS_KEY = 'RabDRmuXXBobanmwwbvpP5LwoG4J8ox34y5Sstz-9jk'
 # -----------------------------------------------
 UNSPLASH_QUERY = 'animal baby'
 # -----------------------------------------------
-ADMIN_BOT_USERNAME = "Saini_extractor_bot" #without @
+ADMIN_BOT_USERNAME = "AbhiTXT1bot" #without @
 
 THUMB_URL = os.environ.get("THUMB_URL", "https://i.ibb.co/JWwQmgvQ/360-F-1056787073-t-K27-Cw-C8-PNCHsnn7lo88-Sz7y4-YFpw-KNd.webp")
 
@@ -38,14 +38,14 @@ THUMB_URL = os.environ.get("THUMB_URL", "https://i.ibb.co/JWwQmgvQ/360-F-1056787
 
 
 # # Bot configuration
-# API_ID = int(os.environ.get("API_ID", "22746239"))
-# API_HASH = os.environ.get("API_HASH", "a98ec8cfd8572a3a7c936cf828fe6215")
-# BOT_TOKEN = os.environ.get("BOT_TOKEN", "7547829346:AAGyfvOu47EciNhC7NUGSDEDFuBaetYYusw")
-# BOT_USERNAME = os.environ.get("BOT_USERNAME", "MassRPBot")
-# OWNER_ID = int(os.environ.get("OWNER_ID", "7463601722"))
-# SUDO_USERS = list(map(int, getenv("SUDO_USERS", "7463601722").split()))
-# CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002601604234"))
+# API_ID = int(os.environ.get("API_ID", "20567114"))
+# API_HASH = os.environ.get("API_HASH", "8090907579:AAE5bN3xZmCbuuj9-NH_IEbWjVpq6awfFrk")
+# BOT_TOKEN = os.environ.get("BOT_TOKEN", "8090907579:AAE5bN3xZmCbuuj9-NH_IEbWjVpq6awfFrk")
+# BOT_USERNAME = os.environ.get("BOT_USERNAME", "AbhiTXT1bot")
+# OWNER_ID = int(os.environ.get("OWNER_ID", "8036182138"))
+# SUDO_USERS = list(map(int, getenv("SUDO_USERS", "8036182138").split()))
+# CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002441127578"))
 # MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://wadiro6523:08AwfhhKRdQaS1i6@cluster0.krzxuop.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-# PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1002601604234"))
+# PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1002441127578"))
 # THUMB_URL = os.environ.get("THUMB_URL", "https://i.fbcd.co/products/original/ug-circle-logo-design-2-e84695ca2ab9a697d2b2d7c928b0bf5f12bf18e076da241815e0372c8d617915.jpg")
 
